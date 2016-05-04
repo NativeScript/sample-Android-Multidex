00b3e3a1502f33664cadd9fa2d9ff4a57a6d6906
Peter Kanev <pip3r4o@gmail.com>
2016-05-04 16:52:36 +0300

    initial commit

---
+++ platforms/android/src/main/assets/app/64kPlusMethodGenerator.js
@@ -0,0 +1,29 @@
+(function() { 
+    var result = "";
+    var methods = 20 * 1000;
+    
+    var package = "package com.tns;\n";
+    var classHeader = "public class RedundantClassNameWithTooManyMethods {\n";
+    var classFooter = "\n}"
+        
+    result += package;    
+    result += classHeader;    
+        
+    var method = " public static void Method0() { }\n";
+    result += method;    
+        
+    for (var index = 1; index < methods; index++) {
+        method = " public static void Method" + index + "() {";
+        method += " Method" + (index-1) + "(); }\n";
+        
+        result += method;     
+    }
+    
+    result += classFooter;    
+    
+    console.log(result);
+    
+    return result;
+})();
+
+// execute with forever start -o RedundantClassNameWithTooManyMethods.java 64kPlusMethodGenerator.js
---
+++ platforms/android/src/main/assets/app/MultiDexApp.js
@@ -0,0 +1,10 @@
+(function(){
+	android.app.Application.extend("com.tns.NativeScriptMultiDexApp", {
+		onCreate: function() {
+		},
+		attachBaseContext: function(base) {
+			this.super.attachBaseContext(base);
+			android.support.multidex.MultiDex.install(this.super);
+		}
+	});
+})();
