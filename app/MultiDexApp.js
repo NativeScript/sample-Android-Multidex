82b3598b817704209ca29a1a2bda32c8243572dc
Plamen5kov <plamen.krasimirov.petkov@gmail.com>
2016-05-09 10:59:01 +0300

    simplified example

--- app/MultiDexApp.js
+++ app/MultiDexApp.js
@@ -1,10 +1,8 @@
-(function(){
-	android.app.Application.extend("com.tns.NativeScriptMultiDexApp", {
-		onCreate: function() {
-		},
-		attachBaseContext: function(base) {
-			this.super.attachBaseContext(base);
-			android.support.multidex.MultiDex.install(this.super);
-		}
-	});
-})();
+android.app.Application.extend("com.tns.NativeScriptMultiDexApp", {
+	onCreate: function() {
+	},
+	attachBaseContext: function(base) {
+		this.super.attachBaseContext(base);
+		android.support.multidex.MultiDex.install(this.super);
+	}
+});
